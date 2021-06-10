2ad179202bf428e0ad1ef18d0c4896ab8da749a1
Vašek Potoček <vasek.potocek@post.cz>
2021-06-10 15:23:45 +0200

    Basic ops: range, first, last, len

--- ops/basic.js
+++ ops/basic.js
@@ -1,11 +1,69 @@
 import { Op, Atom, Stream, InfStream } from '../base.js';
 
+function asnum(s) {
+  if(!(s instanceof Atom))
+    throw 'not atom';
+  const v = s.value;
+  if(typeof v !== 'bigint')
+    throw 'not number';
+  return v;
+}
+
 export class iota extends Op {
-  eval() {
-    const s = new InfStream(this, null);
+  eval(sink) {
+    const s = new InfStream(this, sink);
     let i = 1n;
     s.nextv = () => new Atom(i++);
     s.skip = c => i += c;
     return s;
+  }
+};
+
+export class range extends Op {
+  eval(sink) {
+    const s = new Stream(this, sink);
+    const [min, max] = this.ins[1] && this.ins[2]
+      ? [asnum(this.ins[1]), asnum(this.ins[2])]
+      : [1n, asnum(this.ins[1])];
+    let i = min;
+    s.next = () => i <= max
+        ? { value: new Atom(i++), done: false }
+        : { done: true };
+    s.len = () => max >= min ? max - min + 1n : 0
+    s.last = () => max >= min ? new Atom(max) : null;
+    s.skip = c => i += c;
+    return s;
+  }
+};
+
+export class len extends Op {
+  eval(sink) {
+    if(!this.ins[0])
+      throw 'no arg';
+    return new Atom(this.ins[0].eval(this).len());
+  }
+};
+
+export class first extends Op {
+  eval(sink) {
+    if(!this.ins[0])
+      throw 'no arg';
+    const {value, done} = this.ins[0].eval(this).next();
+    if(done)
+      throw 'first of empty';
+    else
+      return value.eval(sink);
+  }
+};
+
+export class last extends Op {
+  eval(sink) {
+    if(!this.ins[0])
+      throw 'no arg';
+    const l = this.ins[0].eval(this).last();
+    if(l === null)
+      throw 'last of empty';
+    else
+      return l;
   }
 };
--- stream.js
+++ stream.js
@@ -1,9 +1,11 @@
+import {Atom} from './base.js';
 import * as ops from './ops/basic.js';
 
-const o1 = new ops.iota();
+const o1 = new ops.range().plug(new Atom(7), 2).plug(new Atom(-4), 1);
+const o2 = new ops.len().plug(o1);
 
-const iter = o1.eval();
-iter.skip(1000000000000000n);
+/*const iter = o1.eval();
 
 for(const i of iter)
-  console.log(i.desc());
+  console.log(i.desc());*/
+console.log(o2.eval().desc());
